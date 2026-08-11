145fbc0b47d4dfdfcfce77e0163a782eb8bc4c85
Kieran Whelan <83395400+Kieran-Whelan@users.noreply.github.com>
2026-08-11 18:48:47 +0100

    problem16

--- problems/problem15.py
+++ problems/problem15.py
@@ -1,14 +1,2 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
+#lattice paths
 
-num = str(factorial(100))
-
-digits = []
-
-for i in num:
-    digits.append(int(i))
-
-print(sum(digits))
---
+++ problems/problem16.py
@@ -0,0 +1,7 @@
+power = 2**1000
+
+sum = 0
+for i in str(power):
+    sum += int(i)
+
+print(sum)

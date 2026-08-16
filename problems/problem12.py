8a781a6accca5e01516b74b5c242db1a35418c48
Kieran Whelan <83395400+Kieran-Whelan@users.noreply.github.com>
2026-08-16 23:14:25 +0100

    Update problem12.py

--- problems/problem12.py
+++ problems/problem12.py
@@ -1,21 +1,36 @@
-#triangular numbers
+"""
+triangular numbers
+can be computed using t_n = n(n+1)/2
+n and n + 1 are co prime i.e only common divisor is 1
+when n is even number of divisors of t_n = divisors of n/2 * divisors of n+1
+when n is odd number of divisors of t_n = divisors of n * divisors of n+1/2
+"""
 
-nums = []
 
-for i in range(1, 10000 + 1):
-    tri = 0
-    for j in range(1, i + 1):
-        tri += j
-    nums.append(tri)
+def is_even(n):
+    return n % 2 == 0
 
-print("List done")
+def count_divisors(n):
+    count = 0
+    for i in range(1, int(n + 1)):
+        if int(n) % i == 0:
+            count += 1
+    return count
 
-for num in nums:
-    factors = 0
-    if num % 2 == 0:
-        for i in range(1, num + 1):
-            if num % i == 0:
-                factors += 1
-        if factors > 500:
-            print(num)
-            break
+divisors = 0
+n = 1
+triangular_num = 0
+
+while divisors < 500:
+    triangular_num = (n* (n+1))/2
+    if is_even(n):
+        divisors = count_divisors(n/2) * count_divisors(n + 1)
+    else:
+        divisors = count_divisors(n) * count_divisors((n+1)/2)
+    n += 1
+
+print(int(triangular_num))
+print(divisors)
+
+
+
